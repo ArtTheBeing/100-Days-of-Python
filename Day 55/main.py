428c59a20eb0476af2391342ee377cc60932ac60
ArtTheBeing <dmccord9912@gmail.com>
2023-07-06 14:10:55 -0500

    Day 55 Update

--- Day 55/main.py
+++ Day 55/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1 style="text-align: center">Hello, World!</h1>'
 
 @app.route('/bye')
 def say_bye():
@@ -15,4 +15,4 @@ def greet(name, number):
     return f'Hello {name}! you are {number} years old'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) #Run in debug mode
