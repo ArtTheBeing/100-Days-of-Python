b8b83d7b6b814f07fc0ac8fda8b3cf30996ea7dd
ArtTheBeing <dmccord9912@gmail.com>
2023-07-15 14:06:15 -0500

    Day 59 finishup

--- Day 59/upgraded-blog/server.py
+++ Day 59/upgraded-blog/server.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 import requests
 app = Flask(__name__)
 url = 'https://api.npoint.io/481e0423e1501f855ffa'
-
+response = requests.get(url)
+data = response.json()
 
 @app.route('/')
 def home_page():
@@ -20,9 +21,11 @@ def about_page():
 
 @app.route('/post/<int:num>')
 def post(num):
-    response = requests.get(url)
-    data = response.json()
-    return render_template('post.html', num=num, blogs=data)
+    requested_post = None
+    for blog_post in data:
+        if blog_post["id"] == num:
+            requested_post = blog_post
+    return render_template('post.html', num=num, blogs=requested_post)
 
 if __name__ == '__main__':
     app.run(debug=True)
